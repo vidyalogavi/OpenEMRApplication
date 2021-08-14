09b3a5b1e37b0517e31e7142a1d70b0211ed688a
vidyashree.logavi <vidyashree.logavi@192.168.43.184>
2021-08-14 10:23:34 +0530

    base class

--- src/test/java/com/sg/base/WebDriverWrapper.java
+++ src/test/java/com/sg/base/WebDriverWrapper.java
@@ -4,6 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,6 +17,22 @@ public class WebDriverWrapper
 	@BeforeMethod
 	public void setup()
 	{
+		String browser="ch";
+		if(browser.equals("ch"))
+		{
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+			driver = new ChromeDriver();			
+		}
+		else if(browser.equals("ff"))
+		{
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/driver/geckodriver.exe");
+			driver = new FirefoxDriver();			
+		}
+		else if(browser.equals("ie"))
+		{
+			System.setProperty("webdriver.ie.driver", "");
+			driver = new InternetExplorerDriver();
+		}
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
